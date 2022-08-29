2cb616f5629a27b0c77968c7d4853e04c1b4af31
dammyjo <112008172+dammyjo@users.noreply.github.com>
2022-08-29 10:20:03 +0100

    Add files via upload

---
+++ basic calculator.py
@@ -0,0 +1,12 @@
+first_number=30
+second_number=5
+
+adition="+"
+subtraction="-"
+division="/"
+multiplication="*"
+
+print(first_number+second_number)
+print(first_number-second_number)
+print(first_number/second_number)
+print(first_number*second_number)
